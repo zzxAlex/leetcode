660e8173d228e87a7647e84dec767a17b4046238
翟梓修 <zixiu.zhai@lanmaoly.com>
2019-08-28 16:52:48 +0800

    算法2完成

--- src/Code002.java
+++ src/Code002.java
@@ -15,33 +15,26 @@ public class Code002 {
     }
 
     public  ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode(0);
-        ListNode curNode = result;
-        int addNum;//位数
-        int carry = 0;//进位数
+        ListNode root = new ListNode(0);
+        ListNode cursor = root;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+            int sumVal = l1Val + l2Val + carry;
+            carry = sumVal / 10;
 
-        while (null != l1 || null != l2) {
-            addNum = carry + l1.val + l2.val;
-            System.out.println("addNum=" + addNum);
-            if (addNum >= 10) {
-                carry = addNum /10;
-                addNum = addNum %10;
-            }
-            l1 = l1.next;
-            l2 = l2.next;
+            ListNode sumNode = new ListNode(sumVal % 10);
+            cursor.next = sumNode;
+            cursor = sumNode;
 
-            if (null == curNode) {
-                curNode = new ListNode(addNum);
-            } else {
-                curNode.next = new ListNode(addNum);
-            }
-            curNode = curNode.next;
-
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
-        return result.next;
+
+        return root.next;
 
     }
-
     public static void main(String[] args) {
         ListNode listNode1 = new ListNode(2);
         ListNode listNode2 = new ListNode(4);
