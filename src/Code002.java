c05042ae321138fa3fe14ad94e2a53900ef3b6f1
翟梓修 <zixiu.zhai@lanmaoly.com>
2019-08-28 16:38:11 +0800

    算法2完成

--- src/Code002.java
+++ src/Code002.java
@@ -1,34 +1,85 @@
 /**
- * 乐扣001
- * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- *
- * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * 乐扣002
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
  */
 public class Code002 {
 
-    public static void main(String[] args) {
+    public static class ListNode {
+        int val;
+        ListNode next;
 
-        int nums[] = {2,5,5,11};
-        int target = 10;
-        int result[] = twoSum(nums, target);
-        System.out.println(result[0]+"     "+result[1]);
-
-    }
-
-
-    public static  int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            for (int i1 = 1; i1 < nums.length; i1++) {
-                if (target == (nums[i] + nums[i1])&&i!=i1) {
-                    result[0] = i;
-                    result[1] = i1;
-                    return result;
-                }
-            }
+        ListNode(int x) {
+            val = x;
         }
+    }
 
-        return result;
+    public  ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode curNode = result;
+        int addNum;//位数
+        int carry = 0;//进位数
+
+        while (null != l1 || null != l2) {
+            addNum = carry + l1.val + l2.val;
+            System.out.println("addNum=" + addNum);
+            if (addNum >= 10) {
+                carry = addNum /10;
+                addNum = addNum %10;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+
+            if (null == curNode) {
+                curNode = new ListNode(addNum);
+            } else {
+                curNode.next = new ListNode(addNum);
+            }
+            curNode = curNode.next;
+
+        }
+        return result.next;
 
     }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(2);
+        ListNode listNode2 = new ListNode(4);
+        ListNode listNode3 = new ListNode(3);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+
+
+        ListNode listNode4 = new ListNode(5);
+        ListNode listNode5 = new ListNode(6);
+        ListNode listNode6 = new ListNode(4);
+
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
+
+
+        ListNode p = listNode1;
+        ListNode q = listNode4;
+        do {
+            System.out.print(p.val + " ");
+            p = p.next;
+        } while (p != null);
+        System.out.println(" ");
+        do {
+            System.out.print(q.val + " ");
+            q = q.next;
+        } while (q != null);
+        System.out.println(" ");
+
+        ListNode resultNode = new Code002().addTwoNumbers(listNode1, listNode4);
+
+        do {
+            System.out.print(" "+resultNode.val);
+            resultNode = resultNode.next;
+        }while (resultNode != null);
+
+    }
+
+
 }
