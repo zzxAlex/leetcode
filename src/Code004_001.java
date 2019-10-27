2bea3059d76432ebf07613b59077a04c0b667266
翟梓修 <zixiu.zhai@lanmaoly.com>
2019-10-27 22:47:49 +0800

    二分搜索完成

--- src/Code004_001.java
+++ src/Code004_001.java
@@ -24,10 +24,29 @@
 
 
 public class Code004_001 {
-
-
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;//在[l...h]的范围里查找target
+        while (l <= r ) {//当l==h时，区间[l...h]依然有效
+            int mid = l + (r  - l) / 2;//(l+r)/2== l + (r  - l) / 2
+            if (target > nums[mid]) {
+                l = mid + 1;//target在区间[mid+1...r]中
+            }
+            if (target < nums[mid]) {
+                r  = mid - 1;//target在区间[l...mid+1]中
+            }
+            if (target == nums[mid]) {//相等返回下标
+                return mid;
+            }
+        }
+        //没有查找到返回-1。
+        return -l;
+    }
 
     public static void main(String[] args) {
+        int[] num={1,2,4,6,8,9};
 
+
+        System.out.println(new Code004_001().searchInsert(num,8));
     }
 }
