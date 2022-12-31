ebd966d57f2882d6ad6350b2096a7b87e154d903
翟梓修 <zixiu.zhai@lanmaoly.com>
2022-12-31 10:52:03 +0800

    死锁

--- src/Code007.java
+++ src/Code007.java
@@ -3,5 +3,8 @@
  *
  */
 public class Code007 {
+    public static void main(String[] args) {
+        System.out.println("sss");
+    }
 
 }
---
+++ src/DeathLocakTest.java
@@ -0,0 +1,41 @@
+/**
+ * 死锁案例测试
+ */
+public class DeathLocakTest {
+    private final static Object object1 = new Object();
+    private final static Object object2 = new Object();
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(()->{
+            try {
+                synchronized (object1) {
+                    Thread.sleep(1000);
+
+                    synchronized (object2) {
+                        System.out.println("thread 1");
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },"t1");
+
+    Thread thread2 = new Thread(()->{
+        try {
+            synchronized (object2) {
+                Thread.sleep(100);
+
+                synchronized (object1) {
+                    System.out.println("thread 2");
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    },"t2");
+
+    thread1.start();
+        thread2.start();
+}
+
+}
